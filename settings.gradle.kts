b84f0bdd278bc25eaf9c36d4cd1e32282b0649c2
shvetson <shvetson@gmail.com>
2023-03-05 19:01:19 +0300

    update gradle config

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "otus-crowdproj-ratings-2022"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kotestVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        id("io.kotest.multiplatform") version kotestVersion apply false
+    }
+}
+
+include("app")
